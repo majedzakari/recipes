da53b447db41d4d54794d18cc6f200b1817bc54d
majedzakari <majedzakari@users.noreply.github.com>
2016-06-19 01:46:42 +0300

    Update Activity1.cs
    when i changed the playButton in line 28 to button worked for me

--- android/media/audio/play_audio/PlayAudio/Activity1.cs
+++ android/media/audio/play_audio/PlayAudio/Activity1.cs
@@ -10,26 +10,24 @@ using Android.Media;
 
 namespace PlayAudio
 {
-    [Activity (Label = "PlayAudio", MainLauncher = true)]
+    [Activity(Label = "PlayAudio", MainLauncher = true)]
     public class Activity1 : Activity
     {
         MediaPlayer _player;
-        
-        protected override void OnCreate (Bundle bundle)
-        {
-            base.OnCreate (bundle);
 
-            SetContentView (Resource.Layout.Main);
-            
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
             _player = MediaPlayer.Create(this, Resource.Raw.test);
 
-            var playButton = FindViewById<Button> (Resource.Id.playButton);
-            
-            playButton.Click += delegate {
+            var Button = FindViewById<Button>(Resource.Id.PlayButton);
+
+            Button.Click += delegate {
                 _player.Start();
             };
         }
     }
 }
-
-
